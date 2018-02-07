48ba1683bfdd113f10511308db2a8946caa06f6c
leopoldus11 <leopoldarieblau@gmail.com>
2018-02-07 13:38:02 +0100

    added minified css

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,3 @@
 // Copyright Date insertion
 const copyrightDate = new Date().getFullYear();
-console.log(copyrightDate);
-
 document.getElementById('copyright-text').innerHTML = '© ' + copyrightDate;
